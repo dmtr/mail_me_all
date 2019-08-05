eb24749cdb2e4bf81e71e928ad91526e0829f7da
Dmitriy Sorochenkov <podotkos@gmail.com>
2019-08-05 14:38:17 +0300

    Add web server

---
+++ backend/main/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+
+	"github.com/dmtr/mail_me_all/backend/config"
+	"github.com/dmtr/mail_me_all/backend/routes"
+	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
+)
+
+func main() {
+	conf := config.GetConfig()
+	router := gin.Default()
+
+	routes.RegisterRoutes(router)
+
+	server := &http.Server{
+		Addr:    fmt.Sprintf("%s:%d", conf.Host, conf.Port),
+		Handler: router,
+	}
+
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+
+	go func() {
+		<-quit
+		log.Info("Recieve interrupt signal")
+		err := server.Close()
+		if err != nil {
+			log.Errorf("Web server closed : %v", err)
+		}
+
+	}()
+
+	if err := server.ListenAndServe(); err != nil {
+		if err == http.ErrServerClosed {
+			log.Info("Web server shutdown complete")
+		} else {
+			log.Errorf("Web server closed unexpect: %s", err)
+		}
+	}
+	log.Info("Exiting")
+}
