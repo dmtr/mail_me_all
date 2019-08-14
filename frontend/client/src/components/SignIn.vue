2a90b97d969d34504670c870b08625ae0705b2b4
Dmitriy Sorochenkov <podotkos@gmail.com>
2019-08-14 22:53:10 +0300

    Add dummy components

---
+++ frontend/client/src/components/SignIn.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+  <v-facebook-login app-id="2288197271493743"></v-facebook-login>
+</div>  
+</template>
+
+
+<script>
+import { VFBLogin as VFacebookLogin } from 'vue-facebook-login-component';
+
+export default {
+name: "SignIn",    
+components: {
+    VFacebookLogin
+  }
+}
+</script>
+
+<style>
+
+</style>
