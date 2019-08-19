f75b1e1dcee7faa3179aeb6a8c1d0ddc8692bfe9
Dmitriy Sorochenkov <podotkos@gmail.com>
2019-08-19 19:00:29 +0300

    Add dummy route for oauth

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -4,9 +4,15 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+
+	log "github.com/sirupsen/logrus"
 )
 
 //RegisterRoutes setups routes
 func RegisterRoutes(router *gin.Engine) {
 	router.GET("/healthcheck", func(c *gin.Context) { c.String(http.StatusOK, "Ok") })
+	router.GET("/oauth/fb", func(c *gin.Context) {
+		c.String(http.StatusOK, "OK!!!")
+		log.Infof("context %+v", c)
+	})
 }
