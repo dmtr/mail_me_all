c134695fbcc91c8c76e78f188ba9955d8ab46db3
Dmitriy Sorochenkov <podotkos@gmail.com>
2019-09-02 18:55:33 +0300

    Set maxIdleConns and maxOpenConns

--- backend/db/db.go
+++ backend/db/db.go
@@ -8,6 +8,11 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+const (
+	maxIdleConns = 10
+	maxOpenConns = 16
+)
+
 // ConnectDb - try to connect to the database
 func ConnectDb(DSN string, timeout time.Duration) (*sqlx.DB, error) {
 	ticker := time.NewTicker(1 * time.Second)
@@ -22,6 +27,8 @@ func ConnectDb(DSN string, timeout time.Duration) (*sqlx.DB, error) {
 		case <-ticker.C:
 			db, err := sqlx.Connect("postgres", DSN)
 			if err == nil {
+				db.SetMaxIdleConns(maxIdleConns)
+				db.SetMaxOpenConns(maxOpenConns)
 				return db, nil
 			}
 			log.Errorf("failed to connect to db %s %s", DSN, err)
