1285889e7b92ecdf5b92c9c1ae1d262fa35ee3a7
Dmitriy Sorochenkov <podotkos@gmail.com>
2019-09-02 17:40:23 +0300

    Add middlewares

---
+++ backend/middlewares/middlewares.go
@@ -0,0 +1,23 @@
+package middlewares
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/jmoiron/sqlx"
+	log "github.com/sirupsen/logrus"
+)
+
+// TransactionlMiddleware - use for requests that need db transaction
+func TransactionlMiddleware(db *sqlx.DB) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		tx := db.MustBegin()
+		c.Set("Tx", tx)
+		c.Next()
+		if c.IsAborted() {
+			log.Errorln("Context is abortIndex, transaction rollback")
+			tx.Rollback()
+		} else {
+			log.Debugln("Commiting transaction")
+			tx.Commit()
+		}
+	}
+}
