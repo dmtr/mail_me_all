ac1f6c13bdb03314288b9295066d9d2e39874467
Dmitriy Sorochenkov <podotkos@gmail.com>
2019-09-02 17:40:53 +0300

    Add App

---
+++ backend/app/app.go
@@ -0,0 +1,60 @@
+package app
+
+import (
+	"os"
+	"time"
+
+	"github.com/dmtr/mail_me_all/backend/config"
+	"github.com/dmtr/mail_me_all/backend/db"
+	"github.com/dmtr/mail_me_all/backend/routes"
+	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
+)
+
+const (
+	retry time.Duration = 20
+)
+
+// App represents application
+type App struct {
+	Router *gin.Engine
+	Conf   *config.Config
+	Close  func()
+}
+
+func initLogger(loglevel log.Level) {
+	log.SetFormatter(&log.TextFormatter{
+		FullTimestamp: true})
+	log.SetOutput(os.Stdout)
+	if loglevel == 0 {
+		loglevel = log.ErrorLevel
+	}
+	log.SetLevel(loglevel)
+	log.SetReportCaller(true)
+}
+
+// GetApp - returns app
+func GetApp() App {
+	log.Infoln("Loading Config")
+	conf := config.GetConfig()
+	initLogger(conf.Loglevel)
+	log.Infof("Config loaded %v", conf)
+
+	if conf.Debug == 0 {
+		log.Info("Release mode")
+		gin.SetMode(gin.ReleaseMode)
+	}
+
+	db, err := db.ConnectDb(conf.DSN, retry*time.Second)
+	if err != nil {
+		log.Fatalf("Can't connect to database %s", err)
+		os.Exit(1)
+	}
+
+	fn := func() { db.Close() }
+	return App{
+		Router: routes.GetRouter(db),
+		Conf:   &conf,
+		Close:  fn,
+	}
+}
