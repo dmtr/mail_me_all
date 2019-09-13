3bd456d9e4f362887a9143e69a65135c81361d95
Dmitriy Sorochenkov <podotkos@gmail.com>
2019-09-13 13:49:05 +0300

    Get more detailed database error

--- backend/db/user_datastore.go
+++ backend/db/user_datastore.go
@@ -3,7 +3,12 @@ package db
 import (
 	"github.com/dmtr/mail_me_all/backend/models"
 	"github.com/jmoiron/sqlx"
+	"github.com/lib/pq"
 	log "github.com/sirupsen/logrus"
+)
+
+const (
+	uniqueViolationErr pq.ErrorCode = "23505"
 )
 
 type UserDatastore struct {
@@ -19,7 +24,9 @@ func (d *UserDatastore) CreateUser(user *models.User) error {
 	tx := d.DB.MustBegin()
 	_, err := tx.NamedExec("INSERT INTO user_account (name, fb_id, fb_token) VALUES (:name, :fb_id, :fb_token)", user)
 	if err != nil {
-		log.Errorf("Got error inserting user %s", err)
+		if err, ok := err.(*pq.Error); ok {
+			log.Errorf("Got error with code: %s message: %s detail: %s and constraint: %s inserting user: %v", err.Code, err.Message, err.Detail, err.Constraint, user)
+		}
 		return err
 	}
 
