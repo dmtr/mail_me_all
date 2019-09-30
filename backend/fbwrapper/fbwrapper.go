933baa694337fc37a3cb82ceff1c3ec55182cf37
Dmitriy Sorochenkov <podotkos@gmail.com>
2019-09-30 19:50:57 +0300

    Add method to obtain long lived FB token

--- backend/fbwrapper/fbwrapper.go
+++ backend/fbwrapper/fbwrapper.go
@@ -1,8 +1,18 @@
 package fbwrapper
 
 import (
+	"encoding/json"
+	"net/http"
+	"time"
+
 	fb "github.com/huandu/facebook"
 	log "github.com/sirupsen/logrus"
+)
+
+const (
+	fbTokenUrl = "https://graph.facebook.com/v4.0/oauth/access_token"
+	timeout    = time.Duration(10 * time.Second)
+	grantType  = "fb_exchange_token"
 )
 
 func VerifyFbLogin(accessToken string, appID string, appSecret string, FbRedirectURI string) {
@@ -17,4 +27,39 @@ func VerifyFbLogin(accessToken string, appID string, appSecret string, FbRedirec
 		user, _ := session.User()
 		log.Infof("User is %v", user)
 	}
+}
+
+func GenerateLongLivedToken(accessToken string, appID string, appSecret string) (string, error) {
+	req, err := http.NewRequest("GET", fbTokenUrl, nil)
+	if err != nil {
+		log.Errorf("%s", err)
+		return "", err
+	}
+
+	q := req.URL.Query()
+	q.Add("grant_type", grantType)
+	q.Add("client_id", appID)
+	q.Add("client_secret", appSecret)
+	q.Add("fb_exchange_token", accessToken)
+	req.URL.RawQuery = q.Encode()
+
+	client := http.Client{
+		Timeout: timeout,
+	}
+	log.Debugf("Url: %s", req.URL.String())
+	resp, err := client.Do(req)
+	log.Debugf("Response code %s", resp.StatusCode)
+	if err != nil {
+		log.Errorf("%s", err)
+		return "", err
+
+	}
+
+	defer resp.Body.Close()
+
+	var result map[string]interface{}
+	json.NewDecoder(resp.Body).Decode(&result)
+	log.Debugf("result %v", result)
+	return result["access_token"].(string), nil
+
 }
