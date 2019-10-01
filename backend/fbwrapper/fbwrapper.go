74a46cae7f6f2bb7373986cbb4531c295f1b60b6
Dmitriy Sorochenkov <podotkos@gmail.com>
2019-10-01 12:58:44 +0300

    Return user id if access token is valid

--- backend/fbwrapper/fbwrapper.go
+++ backend/fbwrapper/fbwrapper.go
@@ -21,18 +21,11 @@ type AccessTokenResponse struct {
 	ExpiresIn   uint   `json:"expires_in"`
 }
 
-func VerifyFbLogin(accessToken string, appID string, appSecret string, FbRedirectURI string) {
+func VerifyFbToken(accessToken string, appID string, appSecret string, FbRedirectURI string) (userid string, err error) {
 	var globalApp = fb.New(appID, appSecret)
 	globalApp.RedirectUri = FbRedirectURI
 	session := globalApp.Session(accessToken)
-	err := session.Validate()
-	if err != nil {
-		log.Errorf("Invalid access token, got error %s", err)
-	} else {
-		log.Info("Token is valid")
-		user, _ := session.User()
-		log.Infof("User is %v", user)
-	}
+	return session.User()
 }
 
 func GenerateLongLivedToken(accessToken string, appID string, appSecret string) (AccessTokenResponse, error) {
--- backend/main/verify.go
+++ backend/main/verify.go
@@ -6,6 +6,19 @@ import (
 )
 
 func VerifyFbLogin(accessToken string, app *App) {
-	log.Debugf("Verifying token %s", accessToken)
-	fbwrapper.VerifyFbLogin(accessToken, app.Conf.FbAppID, app.Conf.AppSecret, app.Conf.FbRedirectURI)
+	user, err := fbwrapper.VerifyFbToken(accessToken, app.Conf.FbAppID, app.Conf.AppSecret, app.Conf.FbRedirectURI)
+	if err != nil {
+		log.Errorf("Invalid access token: error %s", err)
+	} else {
+		log.Infof("Token is valid, user id %s", user)
+	}
+}
+
+func GenerateFbToken(accessToken string, app *App) {
+	token, err := fbwrapper.GenerateLongLivedToken(accessToken, app.Conf.FbAppID, app.Conf.AppSecret)
+	if err != nil {
+		log.Errorf("Got error %s", err)
+	} else {
+		log.Debugf("Got token %v", token)
+	}
 }
