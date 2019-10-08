f252b6a41faab2169d25d0482ddaa0dc76ca7fa7
Dmitriy Sorochenkov <podotkos@gmail.com>
2019-10-08 17:11:59 +0300

    Call backend on login event

--- frontend/client/src/components/SignIn.vue
+++ frontend/client/src/components/SignIn.vue
@@ -1,16 +1,47 @@
 <template>
   <div>
-    <v-facebook-login app-id="2288197271493743"></v-facebook-login>
+    <v-facebook-login app-id="2288197271493743" v-on:login="login" v-on:logout="logout"></v-facebook-login>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 import { VFBLogin as VFacebookLogin } from "vue-facebook-login-component";
 
 export default {
   name: "SignIn",
   components: {
     VFacebookLogin
+  },
+  methods: {
+    login: function(event) {
+      console.debug("login");
+      if (event.status !== "connected") {
+        return;
+      }
+
+      const data = event.authResponse;
+      const res = axios
+        .post("api/signin/fb", {
+          fbid: data.userID,
+          fbtoken: data.accessToken
+        })
+        .then(function(response) {
+          // handle success
+          console.log(response);
+        })
+        .catch(function(error) {
+          // handle error
+          console.log(error);
+        })
+        .finally(function() {
+          // always executed
+        });
+      console.debug(res);
+    },
+    logout: function(event) {
+      console.debug("logout");
+    }
   }
 };
 </script>
