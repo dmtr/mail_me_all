4f6016d779263548c34a503b820cc9c786f6b16d
Dmitriy Sorochenkov <podotkos@gmail.com>
2019-10-09 17:01:02 +0300

    Add new config parameters

--- backend/config/config.go
+++ backend/config/config.go
@@ -11,6 +11,7 @@ const (
 	appPort     int    = 8080
 	fbProxyPort int    = 5000
 	fbProxyHost string = "fbproxy"
+	maxAge      int    = 60 * 60
 )
 
 // Config - app config
@@ -25,6 +26,13 @@ type Config struct {
 	AppSecret     string
 	FBProxyPort   int
 	FBProxyHost   string
+	Path          string
+	Domain        string
+	MaxAge        int
+	Secure        bool
+	HttpOnly      bool
+	AuthKey       string
+	EncryptKey    string
 }
 
 // GetConfig returns app config
@@ -37,9 +45,13 @@ func GetConfig() Config {
 	viper.SetDefault("DSN", "")
 	viper.SetDefault("FB_APP_ID", "")
 	viper.SetDefault("FB_REDIRECT_URI", "")
-	viper.SetDefault("APP-SECRET", "")
 	viper.SetDefault("FB_PROXY_PORT", fbProxyPort)
 	viper.SetDefault("FB_PROXY_HOST", fbProxyHost)
+	viper.SetDefault("PATH", "/")
+	viper.SetDefault("DOMAIN", "localhost")
+	viper.SetDefault("MAX_AGE", maxAge)
+	viper.SetDefault("SECURE", 0)
+	viper.SetDefault("HTTP_ONLY", 1)
 	viper.AutomaticEnv()
 
 	loglevel, err := log.ParseLevel(viper.GetString("LOGLEVEL"))
@@ -58,6 +70,13 @@ func GetConfig() Config {
 		AppSecret:     viper.GetString("app-secret"),
 		FBProxyPort:   viper.GetInt("FB_PROXY_PORT"),
 		FBProxyHost:   viper.GetString("FB_PROXY_HOST"),
+		Path:          viper.GetString("PATH"),
+		Domain:        viper.GetString("DOMAIN"),
+		MaxAge:        viper.GetInt("MAX_AGE"),
+		Secure:        viper.GetBool("SECURE"),
+		HttpOnly:      viper.GetBool("HTTP_ONLY"),
+		AuthKey:       viper.GetString("auth-key"),
+		EncryptKey:    viper.GetString("encrypt-key"),
 	}
 
 	return conf
