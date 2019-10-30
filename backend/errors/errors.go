2f545a18aee7e52e97e646a85839ed81c5a83c76
Dmitriy Sorochenkov <podotkos@gmail.com>
2019-10-30 16:42:46 +0300

    Remove unused codes

--- backend/errors/errors.go
+++ backend/errors/errors.go
@@ -5,14 +5,12 @@ import "github.com/dmtr/mail_me_all/backend/db"
 type ErrorCode int
 
 const (
-	UnknownError     ErrorCode = iota
-	ServerError      ErrorCode = iota
-	BadRequest       ErrorCode = iota
-	CantGetToken     ErrorCode = iota
-	CantGetUserInfo  ErrorCode = iota
-	DbError          ErrorCode = iota
-	NotFound         ErrorCode = iota
-	CantStartSession ErrorCode = iota
+	UnknownError ErrorCode = iota
+	ServerError  ErrorCode = iota
+	BadRequest   ErrorCode = iota
+	DbError      ErrorCode = iota
+	NotFound     ErrorCode = iota
+	AuthRequired ErrorCode = iota
 )
 
 func GetErrorCode(err error) ErrorCode {
