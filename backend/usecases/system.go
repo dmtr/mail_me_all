574161340694171759109704bc89e995e3bdf1c5
Dmitriy Sorochenkov <podotkos@gmail.com>
2019-11-16 18:56:31 +0300

    Add workers pool

--- backend/usecases/system.go
+++ backend/usecases/system.go
@@ -1,8 +1,14 @@
 package usecases
 
 import (
+	"context"
+	"sync"
+
 	"github.com/dmtr/mail_me_all/backend/models"
 	pb "github.com/dmtr/mail_me_all/backend/rpc"
+	"github.com/google/uuid"
+
+	log "github.com/sirupsen/logrus"
 )
 
 // SystemUseCase implementation
@@ -15,6 +21,31 @@ func NewSystemUseCase(datastore models.UserDatastore, client pb.TwProxyServiceCl
 	return &SystemUseCase{UserDatastore: datastore, RpcClient: client}
 }
 
+func initSubscription(subscriptionID uuid.UUID, datastore models.UserDatastore, wg *sync.WaitGroup) {
+	defer wg.Done()
+	s, err := datastore.GetSubscription(context.Background(), subscriptionID)
+	if err != nil {
+		log.Errorf("Can not get subscription %s, got error %s", subscriptionID, err)
+		return
+	}
+	log.Infof("Got subscription %s", s)
+}
+
 func (s SystemUseCase) InitSubscriptions() error {
-	return nil
+	ctx := context.Background()
+	subscriptions, err := s.UserDatastore.GetNewSubscriptionsIDs(ctx)
+
+	if err != nil {
+		return err
+	}
+	log.Infof("Got subscriptions %s", subscriptions)
+
+	var wg sync.WaitGroup
+	for _, id := range subscriptions {
+		wg.Add(1)
+		go initSubscription(id, s.UserDatastore, &wg)
+	}
+
+	wg.Wait()
+	return err
 }
