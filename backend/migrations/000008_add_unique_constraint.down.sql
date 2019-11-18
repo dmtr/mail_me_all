af092792c0c53430c6d36805edc7e1a46c65d5ff
Dmitriy Sorochenkov <podotkos@gmail.com>
2019-11-18 18:44:59 +0300

    Use twiiter user id instead of primary key

--- backend/migrations/000008_add_unique_constraint.down.sql
+++ backend/migrations/000008_add_unique_constraint.down.sql
@@ -2,4 +2,10 @@ BEGIN;
 
 ALTER TABLE subscription_user_state DROP CONSTRAINT subscription_user_state_unique;
 
+ALTER TABLE subscription_user_state DROP COLUMN user_twitter_id;
+
+ALTER TABLE subscription_user_state ADD COLUMN user_id INTEGER NOT NULL;
+
+ALTER TABLE subscription_user_state ADD CONSTRAINT subscription_user_state_user_id_fk FOREIGN KEY (user_id) REFERENCES subscription_user (id) ON DELETE CASCADE;
+
 COMMIT;
--- backend/migrations/000008_add_unique_constraint.up.sql
+++ backend/migrations/000008_add_unique_constraint.up.sql
@@ -4,4 +4,10 @@ CREATE UNIQUE INDEX subscription_user_state_unique_idx ON subscription_user_stat
 
 ALTER TABLE subscription_user_state ADD CONSTRAINT subscription_user_state_unique UNIQUE USING INDEX subscription_user_state_unique_idx;
 
+ALTER TABLE subscription_user_state DROP COLUMN user_id;
+
+ALTER TABLE subscription_user_state ADD COLUMN user_twitter_id VARCHAR NOT NULL;
+
+ALTER TABLE subscription_user_state ADD CONSTRAINT subscription_user_state_user_id_fk FOREIGN KEY (user_twitter_id) REFERENCES subscription_user (twitter_id) ON DELETE CASCADE;
+
 COMMIT;
