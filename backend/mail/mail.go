447ac322a9b13697817f33878fdd95eda4b99b1b
Dmitriy Sorochenkov <podotkos@gmail.com>
2019-11-27 13:41:49 +0300

    Add mailgun wrapper

---
+++ backend/mail/mail.go
@@ -0,0 +1,32 @@
+package mail
+
+import (
+	"context"
+	"time"
+
+	"github.com/mailgun/mailgun-go/v3"
+	log "github.com/sirupsen/logrus"
+)
+
+const timeout = time.Second * 20
+
+func SendEmail(mgDomain, mgApiKey, from, to, subject, body string) error {
+	var err error
+	mg := mailgun.NewMailgun(mgDomain, mgApiKey)
+
+	m := mg.NewMessage(from, subject, "", to)
+	m.SetHtml(body)
+
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	msg, id, err := mg.Send(ctx, m)
+
+	if err != nil {
+		log.Errorf("Can't send email %s", err)
+	} else {
+		log.Debugf("msg %s, id %s", msg, id)
+	}
+
+	return err
+}
