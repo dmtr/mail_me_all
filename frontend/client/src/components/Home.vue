30da91c20ffa84f9d4c49f02dd02084b064aba23
Dmitriy Sorochenkov <podotkos@gmail.com>
2019-11-28 20:11:06 +0300

    Add Home Component

---
+++ frontend/client/src/components/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div v-if="isUserLoaded">
+    <div v-if="isUserSignedIn">
+      <SubscriptionsList v-bind:subscriptions="subscriptions" />
+    </div>
+    <div v-else>
+      <Welcome />
+    </div>
+  </div>
+  <div v-else>Loading...</div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from "vuex";
+import Welcome from "./Welcome";
+import SubscriptionsList from "./SubscriptionsList";
+
+export default {
+  name: "Home",
+  computed: {
+    ...mapGetters(["isUserSignedIn", "isUserLoaded", "subscriptions"])
+  },
+  components: {
+    Welcome,
+    SubscriptionsList
+  },
+  data: () => ({})
+};
+</script>
