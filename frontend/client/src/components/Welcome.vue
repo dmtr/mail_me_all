846903065e5b128a53a6a2fe7f53a9c5d2be2b01
Dmitriy Sorochenkov <podotkos@gmail.com>
2019-11-29 18:36:08 +0300

    Impove look and feel

--- frontend/client/src/components/Welcome.vue
+++ frontend/client/src/components/Welcome.vue
@@ -1,16 +1,47 @@
 <template>
-  <v-container class="fill-height" fluid>
-    <v-row align="center" justify="center">
-      <v-col class="text-center">
-        <div>Updater makes it possible to get tweets via email.</div>
-      </v-col>
-    </v-row>
-    <v-row align="center" justify="center">
-      <v-col>
+  <v-list dense color="#E1F5FE">
+    <v-list-item>
+      <v-list-item-content
+        class="font-weight-bold"
+      >Updater makes it possible to get tweets via email.</v-list-item-content>
+    </v-list-item>
+    <v-list-item>
+      <v-list-item-content>
+        <v-list dense>
+          <v-subheader>Three simple steps</v-subheader>
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-numeric-1-circle</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>Sign In with Twitter</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-numeric-2-circle</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>Choose users you are interested in</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-numeric-3-circle</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>And get user updates via email</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-list-item-content>
+    </v-list-item>
+    <v-list-item>
+      <v-list-item-content>
         <SignIn />
-      </v-col>
-    </v-row>
-  </v-container>
+      </v-list-item-content>
+    </v-list-item>
+  </v-list>
 </template>
 
 <script>
