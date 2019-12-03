b9949f0c09a67c64dec8de918c9532a74ea63c1d
Dmitriy Sorochenkov <podotkos@gmail.com>
2019-12-03 19:10:56 +0300

    Add entrypoint for cron tasks

---
+++ backend/crontasks/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -euxo pipefail
+
+export MAILME_APP_TEMPLATE_PATH=${MAILME_APP_TEMPLATE_PATH}
+export MAILME_APP_TW_PROXY_HOST=${MAILME_APP_TW_PROXY_HOST}
+export MAILME_APP_DSN=${MAILME_APP_DSN}
+export MAILME_APP_PEM_FILE=${MAILME_APP_PEM_FILE}
+export MAILME_APP_KEY_FILE=${MAILME_APP_KEY_FILE}
+
+CRON_SCHEDULE_CHECK='* * * * *'
+echo "$CRON_SCHEDULE_CHECK /app/mailmeapp check-new-subscriptions" >> /var/spool/cron/crontabs/root
+
+CRON_SCHEDULE_PREPARE='*/2 * * * *'
+echo "$CRON_SCHEDULE_PREPARE /app/mailmeapp prepare-subscriptions" >> /var/spool/cron/crontabs/root
+
+
+CRON_SCHEDULE_SEND='*/3 * * * *'
+echo "$CRON_SCHEDULE_SEND /app/mailmeapp send-subscriptions" >> /var/spool/cron/crontabs/root
+
+crond -f
