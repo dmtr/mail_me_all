32cee4c5d416a850ccda9ca11f9923358c9d70bb
Dmitriy Sorochenkov <podotkos@gmail.com>
2019-12-14 13:53:13 +0300

    Fix: explicitly set domain

--- backend/mail/mail.go
+++ backend/mail/mail.go
@@ -12,7 +12,9 @@ const timeout = time.Second * 20
 
 func SendEmail(mgDomain, mgApiKey, from, to, subject, body string) error {
 	var err error
+
 	mg := mailgun.NewMailgun(mgDomain, mgApiKey)
+	mg.SetAPIBase(mailgun.APIBaseEU)
 
 	m := mg.NewMessage(from, subject, "", to)
 	m.SetHtml(body)
@@ -23,7 +25,7 @@ func SendEmail(mgDomain, mgApiKey, from, to, subject, body string) error {
 	msg, id, err := mg.Send(ctx, m)
 
 	if err != nil {
-		log.Errorf("Can't send email %s", err)
+		log.Errorf("Can't send email, got err %s", err)
 	} else {
 		log.Debugf("msg %s, id %s", msg, id)
 	}
